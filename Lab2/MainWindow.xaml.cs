48e2889f54c92681338a22dd6f3c95e09353ef81
mihan1235 <mihan1235@yandex.ru>
2018-05-01 21:07:57 +0300

    Made a prototype of window.

--- Lab2/MainWindow.xaml.cs
+++ Lab2/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms.DataVisualization;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Lab2
 {
@@ -23,6 +25,23 @@ namespace Lab2
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DrawP2(object sender, RoutedEventArgs e)
+        {
+            // Все графики находятся в пределах области построения ChartArea, создадим ее
+            chart.ChartAreas.Add(new ChartArea("Default"));
+
+            // Добавим линию, и назначим ее в ранее созданную область "Default"
+            chart.Series.Add(new Series("Series1"));
+            chart.Series["Series1"].ChartArea = "Default";
+            chart.Series["Series1"].ChartType = SeriesChartType.Line;
+
+            // добавим данные линии
+            string[] axisXData = new string[] { "a", "b", "c" };
+            double[] axisYData = new double[] { 0.1, 1.5, 1.9 };
+            chart.Series["Series1"].Points.DataBindXY(axisXData, axisYData);
+
         }
     }
 }
